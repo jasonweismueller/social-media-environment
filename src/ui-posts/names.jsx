6c9daf8747a4f12281424ca25cfd4b55aacafed6
Jason Weismueller <60879655+jasonweismueller@users.noreply.github.com>
2026-03-11 22:41:04 +0800

    Add more names for randomization

--- src/ui-posts/names.jsx
+++ src/ui-posts/names.jsx
@@ -1,6 +1,5 @@
 // names.js
 
-
 // -----------------------------
 // INSTAGRAM COMPANY USERNAMES
 // -----------------------------
@@ -11,9 +10,16 @@ export const IG_COMPANY_NAMES = [
   "unitynewsroom", "vistaledger", "northlinepress", "openscopenews",
   "aperturedaily", "frontierbulletin", "pulsewirenews", "clearlinejournal",
   "sentineldigest", "moderndispatch", "factlinebrief", "urbanobserver",
-  "neutralwire", "signalpost", "panoramapress", "dailyverge", "actualnews"
+  "neutralwire", "signalpost", "panoramapress", "dailyverge", "actualnews",
+  "civicscope", "daybreakledger", "globalwatchdaily", "presslinehq",
+  "headlinecurrent", "morningdispatch", "northstarreport", "focusdailynews",
+  "metroheadline", "clarionwire", "todaysignal", "pointviewpress",
+  "newsaxisdaily", "briefingcentral", "nowfrontnews", "cityscopejournal",
+  "northbeaconnews", "worldlinepost", "infocusledger", "publicsquarepress",
+  "alertdailywire", "timelinechronicle", "factwatchdaily", "summitbulletin",
+  "themodernledger", "firstlightnews", "centralverge", "forwardobserver",
+  "primecurrent", "openbriefnews"
 ];
-
 
 // -----------------------------
 // FACEBOOK COMPANY NAMES
@@ -47,9 +53,38 @@ export const FB_COMPANY_NAMES = [
   "Signal Post",
   "Panorama Press",
   "Daily Verge",
-  "Actual News"
+  "Actual News",
+  "Civic Scope",
+  "Daybreak Ledger",
+  "Global Watch Daily",
+  "Pressline HQ",
+  "Headline Current",
+  "Morning Dispatch",
+  "North Star Report",
+  "Focus Daily News",
+  "Metro Headline",
+  "Clarion Wire",
+  "Today Signal",
+  "PointView Press",
+  "NewsAxis Daily",
+  "Briefing Central",
+  "NowFront News",
+  "CityScope Journal",
+  "North Beacon News",
+  "Worldline Post",
+  "InFocus Ledger",
+  "Public Square Press",
+  "Alert Daily Wire",
+  "Timeline Chronicle",
+  "FactWatch Daily",
+  "Summit Bulletin",
+  "The Modern Ledger",
+  "FirstLight News",
+  "Central Verge",
+  "Forward Observer",
+  "Prime Current",
+  "Open Brief News"
 ];
-
 
 // -----------------------------
 // INSTAGRAM USERNAMES
@@ -62,7 +97,15 @@ export const IG_FEMALE_NAMES = [
   "hailey_stevens","rebeccacollins","danielleprice","savannah.myers","chloe_graham",
   "brookelyn.james","isabella_flores","zoemartin","addison_bell","lilyrogers",
   "mackenzie.hill","sydneymorris","emma_howard","faithwatson","juliapierce",
-  "lauryn.tucker","melissawright","aubrey.jenkins","sierra_barnes","delaney.moore"
+  "lauryn.tucker","melissawright","aubrey.jenkins","sierra_barnes","delaney.moore",
+  "paigewilliams","morganreed","claire_henderson","annabrooks","victoriafields",
+  "madelinegray","ellie.jacobs","naomi.collins","lucyward","stella.bryant",
+  "sadie_foster","eva.richards","scarlettyoung","rubyanderson","penelope.bennett",
+  "carolineprice","kennedy_morris","vivian.turner","adelinephillips","clara.evans",
+  "allisonrivera","kelseyward","rileymitchell","gabriella_hayes","mollycooper",
+  "quinnhoward","julietmorgan","harperwhite","willowreed","hadleybarnes",
+  "brooklynnhill","arlia.martin","paytonross","teagan_sanders","elenafisher",
+  "norahking","sloane.peterson","heidiwalker","jadepowell","camilleallen"
 ];
 
 export const IG_MALE_NAMES = [
@@ -73,9 +116,15 @@ export const IG_MALE_NAMES = [
   "dylan_nelson","ericbaker","jordanr","jasonmitchell","connor.evans",
   "brady.morgan","lukepeterson","caleb_thomas","nolanreed","sean.miles",
   "grayson_bennett","noahfisher","tristanward","cameron.murphy","cooper_brooks",
-  "landon.sanders","blakegriffin","owen.hayes","carterwright","isaac.ross"
+  "landon.sanders","blakegriffin","owen.hayes","carterwright","isaac.ross",
+  "evan.parker","adamcole","colinreed","garrett.morris","tysonhoward",
+  "trevormitchell","declan.baker","wesleyyoung","hudsonking","beauadams",
+  "elliotward","roman.thomas","ashermartin","brody.evans","milespeterson",
+  "jaxonhall","cole.bennett","ryderwhite","silaswalker","judefisher",
+  "griffinmoore","prestonallen","grantross","damoncooper","elliotbrooks",
+  "holden.gray","wes.murphy","samuelhayes","parkerreed","gavincole",
+  "brent.sanders","masonphillips","tuckerharris","rowanmartinez","leoviers"
 ];
-
 
 // -----------------------------
 // FACEBOOK REAL NAMES
@@ -90,7 +139,17 @@ export const FB_FEMALE_NAMES = [
   "Chloe Graham","Brookelyn James","Isabella Flores","Zoe Martin",
   "Addison Bell","Lily Rogers","Mackenzie Hill","Sydney Morris",
   "Emma Howard","Faith Watson","Julia Pierce","Lauryn Tucker",
-  "Melissa Wright","Aubrey Jenkins","Sierra Barnes","Delaney Moore"
+  "Melissa Wright","Aubrey Jenkins","Sierra Barnes","Delaney Moore",
+  "Paige Williams","Morgan Reed","Claire Henderson","Anna Brooks",
+  "Victoria Fields","Madeline Gray","Ellie Jacobs","Naomi Collins",
+  "Lucy Ward","Stella Bryant","Sadie Foster","Eva Richards",
+  "Scarlett Young","Ruby Anderson","Penelope Bennett","Caroline Price",
+  "Kennedy Morris","Vivian Turner","Adeline Phillips","Clara Evans",
+  "Allison Rivera","Kelsey Ward","Riley Mitchell","Gabriella Hayes",
+  "Molly Cooper","Quinn Howard","Juliet Morgan","Harper White",
+  "Willow Reed","Hadley Barnes","Brooklynn Hill","Aria Martin",
+  "Payton Ross","Teagan Sanders","Elena Fisher","Norah King",
+  "Sloane Peterson","Heidi Walker","Jade Powell","Camille Allen"
 ];
 
 export const FB_MALE_NAMES = [
@@ -103,5 +162,14 @@ export const FB_MALE_NAMES = [
   "Connor Evans","Brady Morgan","Luke Peterson","Caleb Thomas",
   "Nolan Reed","Sean Miles","Grayson Bennett","Noah Fisher",
   "Tristan Ward","Cameron Murphy","Cooper Brooks","Landon Sanders",
-  "Blake Griffin","Owen Hayes","Carter Wright","Isaac Ross"
+  "Blake Griffin","Owen Hayes","Carter Wright","Isaac Ross",
+  "Evan Parker","Adam Cole","Colin Reed","Garrett Morris",
+  "Tyson Howard","Trevor Mitchell","Declan Baker","Wesley Young",
+  "Hudson King","Beau Adams","Elliot Ward","Roman Thomas",
+  "Asher Martin","Brody Evans","Miles Peterson","Jaxon Hall",
+  "Cole Bennett","Ryder White","Silas Walker","Jude Fisher",
+  "Griffin Moore","Preston Allen","Grant Ross","Damon Cooper",
+  "Elliot Brooks","Holden Gray","Wes Murphy","Samuel Hayes",
+  "Parker Reed","Gavin Cole","Brent Sanders","Mason Phillips",
+  "Tucker Harris","Rowan Martinez","Leo Rivers"
 ];
